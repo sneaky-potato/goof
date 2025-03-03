04bc0fb4e4c222739e8ea98ff14ff8328f891e4a
shawn <ashwanikamal.im421@gmail.com>
2025-03-03 16:51:09 +0530

    Print macros for struct stat

--- stat.c
+++ stat.c
@@ -4,21 +4,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("macro sizeof(stat) %zu end\n", sizeof(struct stat));
+    struct stat stat;
+    printf("macro sizeof(stat) %zu end\n", sizeof(stat));
 
-    st_dev;      
-    st_ino;      
-    st_mode;     
-    st_nlink;    
-    st_uid;      
-    st_gid;      
-    st_rdev;     
-    st_size;     
-    st_blksize;  
-    st_blocks;   
-    st_atim;  
-    st_mtim;  
-    st_ctim;  
+    printf("macro sizeof(stat.st_dev) %zu end\n", sizeof(stat.st_dev));
+    printf("macro sizeof(stat.st_dev) %zu end\n", sizeof(stat.st_dev));      
+    printf("macro sizeof(stat.st_ino) %zu end\n", sizeof(stat.st_ino));      
+    printf("macro sizeof(stat.st_mode) %zu end\n", sizeof(stat.st_mode));     
+    printf("macro sizeof(stat.st_nlink) %zu end\n", sizeof(stat.st_nlink));    
+    printf("macro sizeof(stat.st_uid) %zu end\n", sizeof(stat.st_uid));      
+    printf("macro sizeof(stat.st_gid) %zu end\n", sizeof(stat.st_gid));      
+    printf("macro sizeof(stat.st_rdev) %zu end\n", sizeof(stat.st_rdev));     
+    printf("macro sizeof(stat.st_size) %zu end\n", sizeof(stat.st_size));     
+    printf("macro sizeof(stat.st_blksize) %zu end\n", sizeof(stat.st_blksize));  
+    printf("macro sizeof(stat.st_blocks) %zu end\n", sizeof(stat.st_blocks));   
+    printf("macro sizeof(stat.st_atim) %zu end\n", sizeof(stat.st_atim));  
+    printf("macro sizeof(stat.st_mtim) %zu end\n", sizeof(stat.st_mtim));  
+    printf("macro sizeof(stat.st_ctim) %zu end\n", sizeof(stat.st_ctim));  
 
     return 0;
 }
